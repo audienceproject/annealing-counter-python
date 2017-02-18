cc522ac148b9ef164bbfedcbbed0e64b62df3280
Pimin Konstantin Kefaloukos <kostas@audienceproject.com>
2017-02-18 11:18:22 +0100

    implemented annealing counter

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # annealing-counter-python
-A Python implementation of an annealing counter
+
+A Python implementation of an annealing counter.
+
+## Getting Started
+
+```
+>>> from counter import AnnealingCounter
+>>> c = AnnealingCounter(alpha=0.9)
+>>> c
+Counter value at time 0.0: 0.0
+>>> c.increment()
+>>> c
+Counter value at time 0.0: 1.0
+>>> c.increment(amount=5)
+>>> c
+Counter value at time 0.0: 6.0
+>>> c.increment(t=1.1)
+>>> c
+Counter value at time 1.1: 6.34340399431
+>>> c
+Counter value at time 1.1: 6.34340399431
+>>> c.get_value(10)
+2.4835945910305464
+>>> c.get_value(100)
+0.00018919364956576542
+```
---
+++ counter.py
@@ -0,0 +1,35 @@
+class AnnealingCounter():
+
+    def __init__(self, alpha=0.9):
+        self.alpha = alpha  # rate of decay
+        self.last_t = .0
+        self.heat = .0
+
+    def increment(self, t=None, amount=1.0):
+        """
+        t is a floating point temporal index.
+        If t is not provided, the value of last_t is used
+        """
+        if t is None: t = self.last_t
+        elapsed = t - self.last_t
+        if elapsed < .0 :
+            raise ValueError('Cannot increment the counter in the past, i.e. before the last increment')
+        self.heat = amount + self.heat * (self.alpha ** elapsed)
+        self.last_t = t
+
+    def get_value(self, t=None):
+        """
+        t is a floating point temporal index.
+        If t is not provided, the value of last_t is used
+        """
+        if t is None: t = self.last_t
+        elapsed = t - self.last_t
+        if elapsed < .0 :
+            raise ValueError('Cannot increment the counter in the past, i.e. before the last increment')
+        return self.heat * (self.alpha ** elapsed)
+
+    def __str__(self):
+        return str('Counter value at time {}: {}'.format(self.last_t, self.heat))
+
+    def __repr__(self):
+        return self.__str__()
